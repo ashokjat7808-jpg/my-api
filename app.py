d46c6ef7c8efc0a32ea0c5ab0e0a0385b15861fd
ashokjat7808-jpg <ashokjat7808@gmail.com>
2026-03-28 10:33:24 +0530

    Modify Flask app to use environment variables for host and port
    Updated app to run on host and port from environment variables.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Hello Ashok API is running!"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
