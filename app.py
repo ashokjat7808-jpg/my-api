ec641fe727044e5246fd3324d6cdf6d6a155c3d6
ashokjat7808-jpg <ashokjat7808@gmail.com>
2026-03-28 11:09:18 +0530

    Clean up app.py by removing unused code
    Removed unnecessary imports and commented code for clarity.

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello Ashok API is running!"
-
-# Render के लिए जरूरी
-port = int(os.environ.get("PORT", 10000))
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port)
